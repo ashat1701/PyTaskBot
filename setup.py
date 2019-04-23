df9fba6f005cc3625ad6ac45be25a60f7e33e5a3
ashat1701 <ashat1701@github.com>
2019-04-23 16:35:29 +0300

    fix server

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 webserver.init()
+webserver.app.run()
 
 
 def echo(bot, update):
