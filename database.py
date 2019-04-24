bb2223180a9a46940cd196ac155eda86a1adcce1
ashat1701 <ashat1701@github.com>
2019-04-24 21:52:23 +0300

    add handlers for db

--- database.py
+++ database.py
@@ -1,3 +1,7 @@
+import psycopg2
+import os
+from psycopg2.extras import DictCursor
+
 
 class Database:
     __instance = None
@@ -5,13 +9,23 @@ class Database:
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls, *args, **kwargs)
-            cls.__instance.ids = []
-            cls.__instance.auth_tokens = {}
+            DATABASE_URL = os.environ['DATABASE_URL']
+            cls.__instance.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
         return cls.__instance
 
-    def get_token(self, chat_id):
-        return self.auth_tokens[chat_id]
+    def get_cred(self, chat_id):
+        cursor = self.conn.cursor(cursor_factory=DictCursor)
+        cursor.execute("SELECT * FROM auth WHERE chat_id = %s", (str(chat_id)))
+        return cursor.fetchone()
+
 
     def is_auth(self, chat_id):
-        return chat_id in self.auth_tokens.keys()
-
+        cursor = self.conn.cursor(cursor_factory=DictCursor)
+        cursor.execute("SELECT chat_id FROM auth WHERE chat_id = %s", (str(chat_id)))
+        if len(cursor.fetchone()):
+            cursor.close()
+            return True
+        else:
+            cursor.close()
+            return False
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import auth
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
+
 # TODO: delete this after debug
 def echo(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
@@ -22,12 +23,23 @@ def login(update, context):
         context.bot.send_message(chat_id=chat_id, text=googleAuth.generate_url())
 
 
+def start_callback(update, context, args):
+    if args == "":
+        context.bot.send_message(chat_id=int(update.message.chat_id), text="Здравствуйте! Вам нужно войти в свой аккаунт Google для использования этого бота")
+    else:
+        db = database.Database()
+        chat_id = args
+        if db.is_auth(chat_id):
+            context.bot.send_message(chat_id=int(chat_id), text="Вы успешно вошли в аккаунт")
+
+
 TOKEN = os.environ["TOKEN"]
 PORT = int(os.environ.get('PORT', '8443'))
 updater = telegram.ext.Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, echo))
 dispatcher.add_handler(telegram.ext.CommandHandler('login', login))
+dispatcher.add_handler(telegram.ext.CommandHandler("start", start_callback))
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
