e0922595c92ac1ed1bec7a7304da08c320c46eda
ashat1701 <harmony.moagic@ya.ru>
2019-04-25 23:52:47 +0300

    db fix

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import psycopg2
 import os
-from psycopg2.extras import DictCursor
+from psycopg2.extras import RealDictCursor
 
 
 class Database:
@@ -15,12 +15,12 @@ class Database:
         return cls.__instance
 
     def get_cred(self, chat_id):
-        cursor = self.conn.cursor(cursor_factory=DictCursor)
+        cursor = self.conn.cursor(cursor_factory=RealDictCursor)
         cursor.execute("SELECT * FROM auth WHERE chat_id = %s", (str(chat_id),))
         return cursor.fetchall()[0]
 
     def is_auth(self, chat_id):
-        cursor = self.conn.cursor(cursor_factory=DictCursor)
+        cursor = self.conn.cursor(cursor_factory=RealDictCursor)
         cursor.execute("SELECT chat_id FROM auth WHERE chat_id = %s", (str(chat_id),))
         if cursor.fetchone() is not None:
             cursor.close()
@@ -30,7 +30,7 @@ class Database:
             return False
 
     def delete_cred(self, chat_id):
-        cursor = self.conn.cursor(cursor_factory=DictCursor)
+        cursor = self.conn.cursor(cursor_factory=RealDictCursor)
         cursor.execute("DELETE FROM auth WHERE chat_id = %s", (str(chat_id),))
         self.conn.commit()
         cursor.close()
